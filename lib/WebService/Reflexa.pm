c8a8455c8a21cfcad52534021215f78bc1fe7f4e
Toru Yamaguchi <trymch+public@gmail.com>
2007-05-21 06:43:46 +0000

    Import of ZIGOROU/WebService-Reflexa-0.02 from CPAN.
    gitpan-cpan-distribution: WebService-Reflexa
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ZIGOROU/WebService-Reflexa-0.02.tar.gz
    gitpan-cpan-author:       ZIGOROU
    gitpan-cpan-maturity:     released

--- lib/WebService/Reflexa.pm
+++ lib/WebService/Reflexa.pm
@@ -5,9 +5,11 @@ use warnings;
 
 use base qw(Class::Accessor::Fast);
 
-__PACKAGE__->mk_accessors(qw/rest parser/);
+__PACKAGE__->mk_accessors(qw/rest json xml result/);
 
 use Carp qw(croak);
+use Encode;
+use JSON::Any qw/XS DWIW Syck JSON/;
 use WWW::REST;
 use XML::LibXML;
 
@@ -19,61 +21,97 @@ WebService::Reflexa - Perl wrapper for Japanese assoc word search engine. (http:
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
     my $service = WebService::Reflexa->new;
-    my @words = $service->search('Perl', 'CPAN');
-    print join("\n", @words);
+    my $words = $service->search(['Perl', 'CPAN']);
+    print join("\n", @$words);
 
 =head1 METHODS
 
-=head2 new()
+=head2 new($args)
 
-Create instance, No arguments.
+Create instance, $args is hash reference.
+Key detail is below,
+
+=over 4
+
+=item use_xml (optional)
+
+Default 1. Create L<XML::LibXML> and enable xml response.
+
+=item use_json (optional)
+
+Default 0. Create L<JSON::Any> and enable json response.
+
+=back
 
 =cut
 
 sub new {
-    my $class = shift;
+    my ($class, $args) = @_;
+
+   $args = {
+       use_xml => 1,
+       use_json => 0,
+       (ref $args) ? %$args : ()
+   };
 
     my $self = $class->SUPER::new({
-        parser => XML::LibXML->new,
-        rest => WWW::REST->new($API_URI)
+        xml => ($args->{use_xml}) ? XML::LibXML->new : undef,
+        json => ($args->{use_json}) ? JSON::Any->new : undef,
+        rest => WWW::REST->new($API_URI),
+        result => ''
     });
 
     return $self;
 }
 
-=head2 search(@words)
+=head2 search($word[, $format]);
 
-=head2 search($words_array_ref)
+=head2 search($words_array_ref[, $format])
 
 Search assoc words by keywords.
-Keywords is permitted Array or Array reference.
+Return as array reference.
+Keywords is permitted Array reference or scalar.
+
+If you want to specify format type which is "xml" or "json",
+then you must specify last argument called $format.
 
 =cut
 
 sub search {
-    my $self = shift;
-    my @words = (ref $_[0] eq 'ARRAY') ? @{$_[0]} : @_;
+    my ($self, $words, $format) = @_;
 
-    $self->rest->dispatch($self->dispatcher);
-    return $self->rest->get( q => join(" ", @words), format => "xml");
+    $format ||= ($self->xml) ? "xml" : ($self->json) ? "json" : "";
+    $format = lc($format);
+
+    if ($format eq 'xml') {
+        $self->rest->dispatch($self->xml_dispatcher);
+    }
+    elsif ($format eq 'json') {
+        $self->rest->dispatch($self->json_dispatcher);
+    }
+    else {
+        croak("Unknown format $format or not instanciate $format. Please specify use_$format flag to on in new() arguments");
+    }
+
+    return $self->rest->get( q => join(" ", (ref $words eq 'ARRAY') ? @$words : $words), format => $format);
 }
 
-=head2 dispatcher()
+=head2 xml_dispatcher()
 
 Return code reference for L<WWW::REST>.
 
 =cut
 
-sub dispatcher {
+sub xml_dispatcher {
     my $self = shift;
 
     return sub {
@@ -81,13 +119,72 @@ sub dispatcher {
 
         croak($rest->status_line) if $rest->is_error;
 
-        my $doc = $self->parser->parse_string($rest->content);
+        $self->result($rest->content);
+
+        my $doc = $self->xml->parse_string($self->result);
         my $xc = XML::LibXML::XPathContext->new($doc);
-        my @nodes = map { $_->data } $xc->findnodes("//word/text()");
+        my @nodes = map { encode_utf8($_->data) } $xc->findnodes("//word/text()");
 
         return \@nodes;
     };
 }
+
+=head2 json_dispatcher()
+
+Return code reference for L<WWW::REST>.
+
+=cut
+
+sub json_dispatcher {
+    my $self = shift;
+
+    return sub {
+        my $rest = shift;
+
+        croak($rest->status_line) if $rest->is_error;
+
+        $self->result($rest->content);
+        my $result = [map { encode_utf8($_) } @{$self->json->jsonToObj($self->result)}];
+
+        return $result;
+    };
+}
+
+=head2 rest()
+
+L<WWW::REST> instance.
+
+=head2 json()
+
+L<JSON::Any> instance.
+
+=head2 xml()
+
+L<XML::LibXML> instance.
+
+=head2 result()
+
+Last response string.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item http://labs.preferred.jp/reflexa/
+
+Reflexa search engine top.
+
+=item http://labs.preferred.jp/reflexa/about_api.html
+
+About reflexa API
+
+=item L<WWW::REST>
+
+=item L<XML::LibXML>
+
+=item L<JSON::Any>
+
+=back
 
 =head1 AUTHOR
 
--- t/01-search.t
+++ t/01-search.t
@@ -1,7 +1,22 @@
-use Test::More tests => 2;
+use Test::More tests => 6;
 use WebService::Reflexa;
 
-my $service = WebService::Reflexa->new;
-ok($service);
-my @result = $service->search('ZIGOROu');
-ok(@result >= 0);
+{ ### for xml
+    my $service = WebService::Reflexa->new;
+    ok($service, 'Create instance with xml option');
+
+    ok($service->xml, 'XML::LibXML instance');
+
+    my $result = $service->search(['ZIGOROu']);
+    ok(@$result >= 0, 'XML search');
+}
+
+{ ### for json
+    my $service = WebService::Reflexa->new({ use_json => 1 });
+    ok($service, 'Create instance with json option');
+
+    ok($service->json, 'JSON::Any instance');
+
+    my $result = $service->search(['ZIGOROu'], 'json');
+    ok(@$result >= 0, 'JSON search');
+}
